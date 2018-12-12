ced25e8ebec36bcf6b80d5c355095f01d0d0d9e3
Chad Bramwell <chadbramwell@users.noreply.github.com>
2018-12-11 21:26:55 -0800

    day1 part1 done. I hate what I did. I know there's gotta be a better 'rust-like' way...

--- advent_of_code_2018/day1/src/main.rs
+++ advent_of_code_2018/day1/src/main.rs
@@ -1,20 +1,9 @@
-extern crate atoi;
-
 fn main() {
     //_read_and_print_input();
-    read_and_print_input_line_by_line();
-}
 
-fn _read_and_print_input() {
-    let contents = std::fs::read_to_string("input.txt").expect("failed opening input.txt");
-    println!("{}", contents);
-}
-
-fn read_and_print_input_line_by_line() {
     // options:
-    //  1) read in the entire file as a string, figure out how to split by newline, process each string
-    //  2) read in the entire file as a string, figure out how to get slices to string, pass to atoi
-    //  3) read file as I go, parse as I go, update value as I go
+    //  * read in the entire file as a string, figure out how to get slices to string, pass to atoi
+    //  * read file as I go, parse as I go, update value as I go
     // let contents = std::fs::read_to_string("input.txt").expect("failed opening input.txt");
     // let mut num: s32 = 0;
     // loop {
@@ -22,4 +11,35 @@ fn read_and_print_input_line_by_line() {
     //     num += change;
     //     println!("{}", num);
     // }
+    let sum = parse_whole_file();
+    println!("{}", sum);
+}
+
+fn _read_and_print_input() {
+    let contents = std::fs::read_to_string("input.txt").expect("failed opening input.txt");
+    println!("{}", contents);
+}
+
+fn parse_whole_file() -> i32 {
+    let contents = std::fs::read_to_string("input.txt").expect("failed to open input.txt");
+    let bytes = contents.as_bytes();
+    let mut sum: i32 = 0;
+    let mut last_i = 0;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b'\r' {
+            let slice = &contents[last_i..i];
+            //println!("{}", slice);
+            let value: i32 = slice.parse().expect("parsed integer");
+            println!("{} -> {}", slice, value);
+            sum = sum + value;
+            last_i = i + 2; //+2 for \r\n
+        }
+    }
+    let slice = &contents[last_i..];
+    //println!("{}", slice);
+    let value: i32 = slice.parse().expect("parsed integer");
+    println!("{} -> {}", slice, value);
+    sum = sum + value;
+
+    sum
 }
